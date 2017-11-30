987f3d8af3aaeb6a9b011f2d38bbcf82995be5f1
Jose María Miñambres Redondo <josemariaminambresredondo@iMac-de-Jose.local>
2017-11-30 18:54:41 +0100

    Incluidos bootstrap y font awesome.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modulos/nucleo/home/home.component';
+import { LoginComponent } from './modulos/login/login.component';
+
+const appRoutes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'login', component: LoginComponent}
+];
+@NgModule({
+    imports: [
+        RouterModule.forRoot(appRoutes)
+    ],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './modulos/login/login.component';
+import { HomeComponent } from './modulos/nucleo/home/home.component';
+import { HeaderComponent } from './modulos/nucleo/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
